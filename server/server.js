830433298384a07698f34a059c273b729564dee7
rohith <rohithsai4734@gmail.com>
2025-11-13 11:29:20 +0530

    Updated Code

--- server/server.js
+++ server/server.js
@@ -102,6 +102,6 @@ app.post("/upload", upload.single("file"), async (req, res) => {
     }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
 
